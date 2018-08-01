96b2b0ad00564d886d57dee39e61a1e4307e6394
Zhicai <zzhang2@caltech.edu>
2018-08-01 22:42:40 +0200

    update - clean ups and more scripts

--- cmd.sh
+++ cmd.sh
@@ -1,32 +1,45 @@
 ./Fit2D \
-/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/DelayedPhoton_DoubleEG_2016BCDEFGH_GoodLumi_31p1186ifb.root \
-/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/GMSB_L250TeV_Ctau200cm_13TeV-pythia8.root \
-"L250TeV_Ctau200cm" \
+/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/DelayedPhoton_DoubleEG_2016BCDEFGH_GoodLumi_32p0742ifb.root \
+/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/GMSB_L350TeV_Ctau200cm_13TeV-pythia8.root \
+"L350TeV_Ctau200cm" \
 "signal (L100-Ctau1000)" \
 3J \
 datacard \
-> log_L250TeV_Ctau200cm_datacard.log
+no \
+> log_L350TeV_Ctau200cm_datacard.log
+
+./Fit2D \
+/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim_withBDT/DelayedPhoton_DoubleEG_2016BCDEFGH_GoodLumi_32p0742ifb.root \
+/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim_withBDT/GMSB_L350TeV_Ctau200cm_13TeV-pythia8.root \
+"L350TeV_Ctau200cm" \
+"signal (L100-Ctau1000)" \
+3J \
+datacard \
+yes \
+> log_L350TeV_Ctau200cm_datacard_withBDT.log
 
 
 ./Fit2D \
-/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/DelayedPhoton_DoubleEG_2016BCDEFGH_GoodLumi_31p1186ifb.root \
-/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/GMSB_L250TeV_Ctau200cm_13TeV-pythia8.root \
-"L250TeV_Ctau200cm" \
+/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/DelayedPhoton_DoubleEG_2016BCDEFGH_GoodLumi_32p0742ifb.root \
+/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/GMSB_L350TeV_Ctau200cm_13TeV-pythia8.root \
+"L350TeV_Ctau200cm" \
 "signal (L100-Ctau1000)" \
 3J \
 bias \
+no \
 0.000 \
 10000 \
-> log_L250TeV_Ctau200cm_bias.log
+> log_L350TeV_Ctau200cm_bias.log
 
 ./Fit2D \
-/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/DelayedPhoton_DoubleEG_2016BCDEFGH_GoodLumi_31p1186ifb.root \
-/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/GMSB_L250TeV_Ctau200cm_13TeV-pythia8.root \
-"L250TeV_Ctau200cm" \
+/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/DelayedPhoton_DoubleEG_2016BCDEFGH_GoodLumi_32p0742ifb.root \
+/eos/cms/store/group/phys_susy/razor/Run2Analysis/DelayedPhotonAnalysis/2016/V4p1_private_REMINIAOD/skim/GMSB_L350TeV_Ctau200cm_13TeV-pythia8.root \
+"L350TeV_Ctau200cm" \
 "signal (L100-Ctau1000)" \
 3J \
 binning \
-> log_L250TeV_Ctau200cm_binning.log
+no \
+> log_L350TeV_Ctau200cm_binning.log
 
 
 
